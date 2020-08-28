b7b7f756b5111fc89f187441df29245968c265d8
jianglin <1033553441@qq.com>
2020-08-28 18:22:29 +0800

    create project

---
+++ AVTailor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AVTailor
+//
+//  Created by dfsx6 on 2020/8/28.
+//  Copyright © 2020 tg.ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
